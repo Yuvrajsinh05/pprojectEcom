4e2626cbc6e8239014fe0ef89827780bc9aed2f6
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2023-03-05 15:51:53 +0530

    coonfig

--- backend/node/config.js
+++ backend/node/config.js
@@ -1,5 +1,3 @@
 
 // mongoose.connect("mongodb://localhost:27017/zomato")
 
-const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://weusss:yxpQURxntmCCQUvX@cluster0.kogfpqh.mongodb.net/ecomshop?retryWrites=true&w=majority")
